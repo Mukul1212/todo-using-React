565537c876088b1e3e78978cb592a0830f5260f1
Mukul1212 <102962459+Mukul1212@users.noreply.github.com>
2022-04-29 02:32:08 +0530

    changeComplete

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,23 +1,44 @@
-import React from "react";
+import React ,{useState} from "react";
 
 function App() {
 
 
-  
-  return (
+  const [inputText , setInputText] = useState("");
+  const [items, setItem] = useState([]);
+
+
+  function handleChange (event){
+    const newValue = event.target.value;
+    // console.log(newValue)
+    setInputText(newValue);
+}
+function add(){
+ setItem((preValue) =>{
+   return [
+     ...preValue, inputText
+   ]
+})
+}
+return (
     <div className="container">
       <div className="heading">
         <h1>To-Do List</h1>
       </div>
       <div className="form">
-        <input type="text" />
-        <button>
-          <span>Add</span>
-        </button>
+        <input onChange={handleChange} type="text" value={inputText} />
+        <button onClick={add}>
+  <span>add</span>
+</button>
       </div>
       <div>
         <ul>
-          <li>A Item </li>
+          <li>Bye milk</li>
+           {
+           items.map((todoItem) => {
+              return<li>{todoItem}</li>
+             
+            })
+          } 
         </ul>
       </div>
     </div>
